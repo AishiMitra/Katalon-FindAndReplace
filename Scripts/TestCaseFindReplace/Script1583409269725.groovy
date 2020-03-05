60f220c0efaae39f9b9155319d1058afcc34a4be
user <user@192.168.2.44>
2020-03-05 17:29:13 +0530

    First Commit

---
+++ Scripts/TestCaseFindReplace/Script1583409269725.groovy
@@ -0,0 +1,22 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+String Str = 'Hi.. Her name is aishi mitra. aishi is a good girl, aishi live in kolkata. aishi loves to eat chicken. Be like aishi.'
+
+String newDoc = CustomKeywords.'com.katalon.FindReplace.getDocToReplace'(Str, 'aishi', 'shila')
+
+println(newDoc)
